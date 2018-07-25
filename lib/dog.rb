66d0001a2d6d3b7c0d800ce98c6649824310efa0
Caroline Berg <carolineeberg@gmail.com>
2018-07-25 15:15:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=("woof!")
+    @bark = "woof!"
+  end
+
+  def bark
+    @bark
+  end
+
+  fido = Dog.new
+  fido.name = "Fido"
+  puts fido.bark
